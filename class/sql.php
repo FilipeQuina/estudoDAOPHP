98378c4b36cfa10d357eb30da012324be2906731
FilipeQuina <filipe.pacheco00@gmail.com>
2018-02-01 16:10:27 -0200

    iniciamos o projeto

---
+++ class/sql.php
@@ -0,0 +1,28 @@
+<?php
+
+class Sql extends PDO{
+    private $conn;
+    public function __construct(){
+        $this->conn = new PDO("mysql:host=localhost;dbname=bdphp7","root","");
+    }
+    private function setParams($statment, $parameters = array()){
+        foreach ($parameters as $key => $value) {
+            $statment->bindParam($key, $value);
+        }
+    }
+    private function setParam($statment, $key, $value){
+        $statment->bindParam($key,$value);
+    }
+    public function query ($rawQuery,$params = array()){
+        $stmt = $this->conn->prepare($rawQuery);
+            $this->setParams($stmt,$params);
+            $stmt->execute();
+            return $stmt;
+    }
+    public function select($rawQuery, $params = array()):array{
+        $stmt = $this->query($rawQuery, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
